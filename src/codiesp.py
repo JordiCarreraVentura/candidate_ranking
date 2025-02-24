39fac0073447167592ca8690311a013e45905432
JordiCarreraVentura <jordi.carrera.ventura@gmail.com>
2025-02-24 08:46:30 +0100

    Implement test set

--- src/codiesp.py
+++ src/codiesp.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import pandas as pd
 from nltk import sent_tokenize as splitter
@@ -18,6 +19,12 @@ PATH_CODIESP = os.path.join(PROJECT_HOME, "data", "final_dataset_v2_to_publish")
 PATH_CODIESP_TRAIN = os.path.join(PATH_CODIESP, "train")
 PATH_CODIESP_TRAIN_FILES = os.path.join(PATH_CODIESP_TRAIN, "text_files_en")
 
+PATH_CODIESP_TEST = os.path.join(PATH_CODIESP, "test")
+PATH_CODIESP_TEST_FILES = os.path.join(PATH_CODIESP_TEST, "text_files_en")
+
+
+# Training set
+
 sentence_rows = []
 for idx, txt_file in enumerate(tqdm(os.listdir(PATH_CODIESP_TRAIN_FILES))):
     for sent in splitter(from_txt(os.path.join(PATH_CODIESP_TRAIN_FILES, txt_file))):
@@ -25,3 +32,18 @@ for idx, txt_file in enumerate(tqdm(os.listdir(PATH_CODIESP_TRAIN_FILES))):
 
 CODIESP_SCHEMA = ["text_path", "text_id", "sentence"]
 CODIESP = pd.DataFrame(sentence_rows, columns=CODIESP_SCHEMA)
+CODIESP["text_id"] = CODIESP["text_id"].astype('int')
+
+
+# Test set
+
+sentence_rows_test = []
+for idx, txt_file in enumerate(tqdm(os.listdir(PATH_CODIESP_TEST_FILES))):
+    for sent in splitter(from_txt(os.path.join(PATH_CODIESP_TEST_FILES, txt_file))):
+        sentence_rows_test.append((txt_file, idx, sent))
+
+CODIESP_SCHEMA = ["text_path", "text_id", "sentence"]
+TEST = pd.DataFrame(sentence_rows_test, columns=CODIESP_SCHEMA)
+random.seed(400449)
+TEST = TEST.sample(10000)
+TEST["text_id"] = TEST["text_id"].astype('int')
